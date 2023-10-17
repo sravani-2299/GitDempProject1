805b404fc5a951034eb4859b7b219a06c761b42b
sravani-2299 <127007448+sravani-2299@users.noreply.github.com>
2023-10-17 11:58:19 +0530

    Commiting Test2 to BranchX

---
+++ src/Test2.java
@@ -0,0 +1,19 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=3;
+		if(a>b)
+		{
+			System.out.println(a);
+		}
+		else
+		{
+			System.out.println(b);
+		}
+
+	}
+
+}
